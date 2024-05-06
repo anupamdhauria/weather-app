61cdc4ced22c6dc4fcd3ea893d1eb9940f0dc544
anupamdhauria <anupamdhauria97@gmaul.com>
2024-05-06 16:21:42 +0530

    changes

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -7,7 +7,9 @@ const Card=()=>{
             <img src={weather.data?.current?.condition?.icon}/>
             <h3>{weather.data?.current?.condition?.text}</h3>
             <h2>{weather.data?.current?.temp_c} C</h2>
-            <h5>{weather.data?.location?.name}{" ,"}{weather.data?.location?.country}</h5>
+            <h5>{weather.data?.location?.name}{" ,"}
+                {weather.data?.location?.region}{","}
+            {weather.data?.location?.country}</h5>
         </div>
     );
 };
